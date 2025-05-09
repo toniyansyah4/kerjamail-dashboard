f4e6ab5a8f46361684e699ac029a95605286881f
toniyansyah4 <toniyansyah205@gmail.com>
2025-05-09 17:17:51 +0700

    first commit

---
+++ database/migrations/2025_05_09_095331_create_domains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->enum('status', ['pending', 'active', 'invalid']);
+            $table->json('dns_records')->nullable(); // Untuk MX, SPF, DKIM, etc
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('domains');
+    }
+};
---
+++ database/migrations/2025_05_09_095407_create_mailboxes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mailboxes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('domain_id')->constrained()->onDelete('cascade');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('storage_quota')->default(500); // dalam MB
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mailboxes');
+    }
+};
